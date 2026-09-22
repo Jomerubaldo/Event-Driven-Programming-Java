d362eaf6e1fa5754936185bd965ff8be2090dbb6
Your Name <jomerubaldo@gmail.com>
2026-09-22 11:54:58 +0800

    initial commit

---
+++ Week02/Exercise4.java
@@ -0,0 +1,30 @@
+/*Write a program that checks whether a given string starts with a vowel (a, e, i, o, u).  Return true if the string starts with a vowel, otherwise false.  */
+
+package Week02;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input1 = scanner.nextLine().toLowerCase();
+
+        String[] vowel = {"a", "e", "i", "o", "u"};
+        boolean isVowel = false;
+
+       if (Arrays.asList(vowel).contains(input1)) {
+        isVowel = true;
+        System.out.println("Starts with " +  "'" + input1 + "'" + " " + "vowel?" + " " + isVowel );
+       }
+
+        scanner.close();
+
+    }
+    
+}
+
+// Explain
+
+/*First kuha din ng input1 ni user tapos declared na array list na may value na vowel at declared din ng boolean ito yung magsasabi kung true or false ba ang inimput ni user based sa mga value na naka stored sa arraylist tapos conditional statement lang na kung yung array.asList yung mismong list ng array na may value which is yung vowel na variable is may contains or may laman na inimput ni user na based sa naka stored sa arraylist tapos kapag true mapapalitan yung value ng isVowel na boolean to true if otherwise false tapos print lang ang result  */
---
+++ Week02/Exercise5.java
@@ -0,0 +1,11 @@
+/*. Write a program that takes a string input ("true" or "false") and converts it to a boolean  value. The program should handle case insensitivity (e.g., "TRUE", "false"). */ 
+
+package Week02;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        System.out.println("Hello Morld!");
+
+    }
+    
+}
